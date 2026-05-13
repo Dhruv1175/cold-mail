b637787ae83c51357ea78769aa7ce5d52240c471
Dhruv <dhruvgajra7@gmail.com>
2026-05-13 18:39:28 +0530

    scraper fix

--- services/scraper.ts
+++ services/scraper.ts
@@ -7,7 +7,7 @@ import { cleanDomain } from "@/lib/utils";
 
 export async function scrapeYCombinator(): Promise<void> {
   console.log("[SCRAPER] Starting...");
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
 
   try {
